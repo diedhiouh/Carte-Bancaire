0d3e3bae38639ffd0f0f3b31a28a3730e749f1c8
Habibou DIEDHIOU <habibodh@gmail.com>
2019-03-24 23:08:05 +0000

    Classe Visa implemente et ajout de style

--- carte.js
+++ carte.js
@@ -1,3 +1,4 @@
+window.onload=init;
 //La Classe Banque
 class Banque{
 	constructor(NumCarte){
@@ -11,12 +12,13 @@ class Banque{
 	}
 }
 
+
 //La Classe Verificateur qui verifie les cartes
 class Verificateur extends Banque{
 	constructor(NumCarte){
 		super(NumCarte);
 	}
-		verifier(){
+		verifier(NumCarte){
 			throw new Error('You must implement this method');
 		  }
 	}
@@ -26,9 +28,52 @@ class Visa extends Verificateur{
 	constructor(NumCarte){
 		super(NumCarte);
 	}
-	verifier () {
-		console.log("Je suis un Visa");
+	
+verifier (value) {
+	var result="";
+	var value=this.NumCarte;
+	if(value.length!=15){
+		var debut=value.slice(0,2);
+		if(debut!="14" || debut!="15"){
+			
+			// takes the form field value and returns true on valid number
+			function valid_credit_card(value) {
+			// accept only digits, dashes or spaces
+				if (/[^0-9-\s]+/.test(value)) return false;
+
+			// The Luhn Algorithm. It's so pretty.
+				var nCheck = 0, nDigit = 0, bEven = false;
+				value = value.replace(/\D/g, "");
+
+				for (var n = value.length - 1; n >= 0; n--) {
+					var cDigit = value.charAt(n),
+						nDigit = parseInt(cDigit, 10);
+
+					if (bEven) {
+						if ((nDigit *= 2) > 9) nDigit -= 9;
+					}
+
+					nCheck += nDigit;
+					bEven = !bEven;
+				}
+
+				if((nCheck % 10) == 0){
+					//alert("Valide");
+					result=true;
+					let mes="La carte est du type VISA";
+					document.getElementById("message").value=mes;
+				}else{
+					//alert("invalide");
+					result=false;
+					let err="La carte est invalide";
+					document.getElementById("message").value=err;
+			}
+		}
+		var r=valid_credit_card(this.NumCarte);
+		alert(result);
+		}
 	}
+}
 }
 
 //Classe MasterCard verifie si la carte est du type MasterCard
@@ -51,7 +96,22 @@ class AmericanExp extends Verificateur{
 	}
 }
 
-//LA CLASSE PRINCIPALE
-let banq=new Banque("568985696");
-banq=new Visa("8498989");
-banq.verifier();
+/*LA CLASSE PRINCIPALE*/
+function init(){
+	var n=document.getElementById('valid');
+	n.onclick=Affiche;
+}
+function Affiche(){
+	var num=recup();
+	//let banq=new Banque("568985696");
+	//let banq=new Visa("14485275742308327");
+	var cart=recup();
+	alert(cart);
+	let banq=new Visa(cart);
+	banq.verifier();
+}
+function recup(){
+	var credit=document.getElementById('entrer').value;
+	return credit;
+}
+
